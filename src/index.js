484d8c55eae5ee88616096014a54d30239c4213c
KiosaOleg <117593116+KiosaOleg@users.noreply.github.com>
2023-04-30 20:21:05 +0200

    hw11

---
+++ src/index.js
@@ -0,0 +1,117 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import PixabayApiService from './js/pixabay-service';
+import LoadMoreBtn from './js/load-more-btn';
+
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  searchInput: document.querySelector('.search-input'),
+  searchBtn: document.querySelector('search-btn'),
+  galleryCards: document.querySelector('.gallery'),
+  body: document.querySelector('body'),
+};
+refs.body.style.backgroundColor = '#e2e0dc';
+
+const pixabayApiService = new PixabayApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+loadMoreBtn.refs.button.hidden = true;
+
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetnchArticles);
+
+function onSearch(event) {
+  event.preventDefault();
+
+  pixabayApiService.query = event.currentTarget.elements.query.value;
+  clearArticlesContainer();
+
+  if (pixabayApiService.query === '') {
+    Notify.info('Please, write something...');
+  } else {
+    clearArticlesContainer();
+
+    loadMoreBtn.Show();
+    pixabayApiService.resetPage();
+    loadMoreBtn.refs.button.hidden = false;
+    Notify.success('Here are the images we were able to find!');
+    fetnchArticles();
+  }
+}
+
+function fetnchArticles() {
+  loadMoreBtn.disable();
+  pixabayApiService.fetchArticles().then(hits => {
+    createGalleryMarkup(hits);
+    loadMoreBtn.enable();
+  });
+}
+
+function createGalleryMarkup(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+    <div class="photo-card">
+      <a href="${largeImageURL}">
+        <img
+          class="photo-card__img"
+          src="${largeImageURL}" 
+          alt="${tags}" 
+          loading="lazy" 
+          width="320"
+          height="212"
+        />
+      </a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          <span>${likes}</span>
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          <span>${views}</span>
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          <span>${comments}</span>
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          <span>${downloads}</span>
+        </p>
+      </div>
+    </div>
+    `;
+      }
+    )
+    .join('');
+
+  refs.galleryCards.insertAdjacentHTML('beforeend', markup);
+  gallery.refresh();
+}
+
+function clearArticlesContainer() {
+  refs.galleryCards.innerHTML = '';
+}
+
+var gallery = new SimpleLightbox('.gallery a', {
+  /* options */
+});
